38773719025df2c8a86db7e47a6afe3d02325c76
Van Phan <VanPhan514@gmail.com>
2016-03-01 18:37:30 -0500

    Added modals

--- public/javascripts/angularDemo/RedmineController.js
+++ public/javascripts/angularDemo/RedmineController.js
@@ -1,7 +1,10 @@
 angular.module('redmine')
-.controller('RedmineController', function($scope, project) {
+.controller('RedmineController', function($scope, project, $uibModal, $log) {
+
   $scope.parents = [];
   $scope.subs = [];
+
+  //Grabs all projects and parse parents and subs
   project.all()
   .success(function(data) {
     for (var i = 0; i < data.projects.length; i++) {
@@ -15,4 +18,18 @@ angular.module('redmine')
     console.log($scope.parents);
     console.log($scope.subs);
   });
+
+  //Open modal
+  $scope.issues = function (size) {
+
+    var modalInstance = $uibModal.open({
+      animation: $scope.animationsEnabled,
+      templateUrl: 'http://redmine.micnguyen.com/AngularRedmineDemo/templates/modal.html',
+      controller: function ($scope, $uibModalInstance) {
+        
+      },
+      size: size
+    });
+  };
+
 });
