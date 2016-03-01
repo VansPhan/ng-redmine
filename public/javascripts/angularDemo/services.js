0f61286ab16f1402a06283d3a7d44c453fef66ea
Van Phan <vanphan514@gmail.com>
2016-03-01 04:56:38 -0500

    Working copy without modals

--- public/javascripts/angularDemo/services.js
+++ public/javascripts/angularDemo/services.js
@@ -2,7 +2,7 @@ angular.module('redmine-services', [])
 .factory('project', ['$http', function projectFactory($http) {
   return {
     all: function() {
-      return $http({ method: 'GET', url: 'http://redmine.micnguyen.com/projects.json' });
+      return $http({ method: 'GET', url: 'http://redmine.micnguyen.com/projects.json?limit=60' });
     }
   };
 }]);
